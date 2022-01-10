5d8d29f7cd1e583968d557cc866d25879925f979
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-10 20:06:54 +0100

    Clean up in code.

---
+++ Pokemon2/Utils.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+#include "Utils.h"
+
+using std::cin;
+
+/*
+    Press enter thanks to: https://stackoverflow.com/a/903230/13521548
+*/
+void pressEnter() {
+    cin.ignore();
+}
+
+/*
+    Clearing screen: https://stackoverflow.com/a/5866648/13521548
+*/
+
+void clearScreen() {
+    COORD tl = { 0,0 };
+    CONSOLE_SCREEN_BUFFER_INFO s;
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(console, &s);
+    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
+    FillConsoleOutputCharacter(console, ' ', cells, tl, &written);
+    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
+    SetConsoleCursorPosition(console, tl);
+}
+
+void pressEnterAndClear() {
+    pressEnter();
+    clearScreen();
+}
---
+++ Pokemon2/Utils.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void pressEnter();
+void clearScreen();
+void pressEnterAndClear();
