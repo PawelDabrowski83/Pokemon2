6a867b46686983f86caade9239a7f377f8b6ddb6
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-10 20:02:17 +0100

    Add additional work on main screen.

--- Pokemon2/Messages.h
+++ Pokemon2/Messages.h
@@ -4,6 +4,11 @@
 using std::string;
 
 const string MAIN_INTRODUCTION = "Welcome to text-based experience called...";
+
+/*
+Solution for multiline strings: https://stackoverflow.com/a/55689576/13521548
+Ascii art from: https://patorjk.com/software/taag/#p=display&f=JS%20Stick%20Letters&t=Grand
+*/
 const char* GRAND_TITLE = R""""(
          __   __             __  
         / _` |__)  /\  |\ | |  \ 
@@ -13,5 +18,5 @@ ___  __        __                   ___      ___
  |  /  \ |  | |__) |\ |  /\   |\/| |__  |\ |  |  
  |  \__/ \__/ |  \ | \| /~~\  |  | |___ | \|  |
 
-)""""; // thanks to https://patorjk.com/software/taag/#p=display&f=JS%20Stick%20Letters&t=Grand
+)""""; 
 const string COMMAND_ENTER = "Press [ENTER] to continue.";
--- Pokemon2/Pokemon2.cpp
+++ Pokemon2/Pokemon2.cpp
@@ -1,17 +1,47 @@
 ﻿#include <iostream>
-#include <limits>
 #include "Messages.h"
-
+#include <windows.h>
 
 using std::cout;
 using std::endl;
 using std::cin;
-using std::streamsize;
+using std::string;
+
+void pressEnter();
+void pressEnterAndClear();
+void clearScreen();
 
 int main()
 {
     cout << MAIN_INTRODUCTION << endl;
     cout << GRAND_TITLE << endl;
     cout << COMMAND_ENTER << endl;
-    cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+    pressEnterAndClear();
+}
+
+/*
+    Press enter thanks to: https://stackoverflow.com/a/903230/13521548
+*/
+void pressEnter() {
+    cin.ignore();
+}
+
+/*
+    Clearing screen: https://stackoverflow.com/a/5866648/13521548
+*/
+
+void clearScreen() {
+    COORD tl = { 0,0 };
+    CONSOLE_SCREEN_BUFFER_INFO s;
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(console, &s);
+    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
+    FillConsoleOutputCharacter(console, ' ', cells, tl, &written);
+    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
+    SetConsoleCursorPosition(console, tl);
+}
+
+void pressEnterAndClear() {
+    pressEnter();
+    clearScreen();
 }
