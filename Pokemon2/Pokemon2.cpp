c42d7ab48eff78c7d4dcf32261ab9036891c68ff
pawel.d.dabrowski@gmail.com <pawel.d.dabrowski@gmail.com>
2022-01-10 17:45:18 +0100

    Add welcome screen and press ENTER command.

--- Pokemon2/Pokemon2.cpp
+++ Pokemon2/Pokemon2.cpp
@@ -1,11 +1,17 @@
 ﻿#include <iostream>
+#include <limits>
 #include "Messages.h"
+
 
 using std::cout;
 using std::endl;
+using std::cin;
+using std::streamsize;
 
 int main()
 {
     cout << MAIN_INTRODUCTION << endl;
-    cout << GRAND_TITLE << std::endl;
+    cout << GRAND_TITLE << endl;
+    cout << COMMAND_ENTER << endl;
+    cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
 }
